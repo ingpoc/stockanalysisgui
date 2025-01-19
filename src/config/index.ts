24a77a63a2cab7536971a64a33a6713c7a3d82fb
Gurusharan Gupta <gupta.rp.gurusharan@gmail.com0>
2025-01-19 20:23:15 +0530

    Enhance configuration and context management for multichain support
    - Updated the configuration to include multiple networks (mainnet, arbitrum, solana, solanaDevnet, solanaTestnet) and ensured the project ID is defined.
    - Integrated Solana wallet adapters (HuobiWalletAdapter, SolflareWalletAdapter) into the Solana web3 adapter.
    - Refactored the ContextProvider to utilize the new modal for wallet connections and improved metadata handling for the application.
    - Enhanced the AppKit instance setup to include analytics features and a light theme mode.
    
    These changes contribute to a more robust multichain integration, improved user experience, and better configuration management.

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,22 +1,35 @@
 import { cookieStorage, createStorage } from 'wagmi'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { mainnet } from '@reown/appkit/networks'
+import { mainnet, arbitrum, solana, solanaDevnet, solanaTestnet } from '@reown/appkit/networks'
 import { createAppKit } from '@reown/appkit/react'
+import type { AppKitNetwork } from '@reown/appkit/networks'
+import { HuobiWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { SolanaAdapter } from '@reown/appkit-adapter-solana/react'
 
-const APP_URL = process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID || "09fccb634332c292a3cb889c0592b928"
+
+if (!projectId) {
+  throw new Error('Project ID is not defined')
+}
+
+export const networks = [mainnet, arbitrum, solana, solanaDevnet, solanaTestnet] as [AppKitNetwork, ...AppKitNetwork[]]
 
 // Set up the Wagmi Adapter with cookie storage
 export const wagmiAdapter = new WagmiAdapter({
   storage: createStorage({
-    storage: cookieStorage,
+    storage: cookieStorage
   }),
   ssr: true,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID || '',
-  networks: [mainnet]
+  projectId,
+  networks
+})
+
+export const solanaWeb3JsAdapter = new SolanaAdapter({
+  wallets: [new HuobiWalletAdapter(), new SolflareWalletAdapter()]
 })
 
 // Create the AppKit instance
-export const appKit = createAppKit({
+/*export const appKit = createAppKit({
   adapters: [wagmiAdapter],
   projectId: process.env.NEXT_PUBLIC_PROJECT_ID || '',
   networks: [mainnet],
@@ -28,7 +41,7 @@ export const appKit = createAppKit({
     url: APP_URL,
     icons: [`${APP_URL}/icon.svg`]
   }
-})
+})*/
 
 // Export wagmi config for use in other parts of the application
-export const config = wagmiAdapter.wagmiConfig 
+export const config = wagmiAdapter.wagmiConfig
